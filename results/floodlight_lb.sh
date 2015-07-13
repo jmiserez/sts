8d8792654c3c149e1494c01430b852bbe3ac4288
Jeremie Miserez <jeremie@miserez.org>
2015-07-13 16:02:25 +0200

    Update Floodlight load balancer configuration file

--- results/floodlight_lb.sh
+++ results/floodlight_lb.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-curl -X POST -d '{"id":"1","name":"vip1","protocol":"icmp","address":"10.0.0.100","port":"8"}' http://localhost:8080/quantum/v1.0/vips/
+curl -X POST -d '{"id":"1","name":"vip1","protocol":"icmp","address":"10.0.0.99","port":"8"}' http://localhost:8080/quantum/v1.0/vips/
 curl -X POST -d '{"id":"1","name":"pool1","protocol":"icmp","vip_id":"1"}' http://localhost:8080/quantum/v1.0/pools/
-curl -X POST -d '{"id":"1","address":"123.123.1.2","port":"8","pool_id":"1"}' http://localhost:8080/quantum/v1.0/members/
-curl -X POST -d '{"id":"2","address":"123.123.1.3","port":"8","pool_id":"1"}' http://localhost:8080/quantum/v1.0/members/
+curl -X POST -d '{"id":"1","address":"123.123.2.2","port":"8","pool_id":"1"}' http://localhost:8080/quantum/v1.0/members/
+curl -X POST -d '{"id":"2","address":"123.123.2.3","port":"8","pool_id":"1"}' http://localhost:8080/quantum/v1.0/members/
 
