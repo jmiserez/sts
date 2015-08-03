fc585efd8508a8bfa5e9596e7f1bd49fa04bbba5
Jeremie Miserez <jeremie@miserez.org>
2015-08-03 16:24:05 +0200

    Add hb_process.sh to /tools. Process a directory of traces and create dot files.

---
+++ tools/hb_process.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+SCRIPT=$(readlink -f $0)
+SCRIPTPATH=`dirname $SCRIPT`
+(
+	pushd "$1";
+	(
+		"$SCRIPTPATH/../sts/happensbefore/hb_graph.py" hb.json > hb.out && dot -Tpdf hb.dot -o hb.pdf && xdot hb.dot
+	);
+	popd
+)
