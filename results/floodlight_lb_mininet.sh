d05b284e3fe22be914f805c7615d1cedfa4d442e
Jeremie Miserez <jeremie@miserez.org>
2015-08-03 15:19:12 +0200

    Remove "results" folder, commit files to separate repository

--- results/floodlight_lb_mininet.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
- 
-curl -X POST -d '{"id":"1","name":"vip1","protocol":"icmp","address":"10.0.0.100","port":"8"}' http://localhost:8080/quantum/v1.0/vips/
-curl -X POST -d '{"id":"1","name":"pool1","protocol":"icmp","vip_id":"1"}' http://localhost:8080/quantum/v1.0/pools/
-curl -X POST -d '{"id":"1","address":"10.0.0.2","port":"8","pool_id":"1"}' http://localhost:8080/quantum/v1.0/members/
-curl -X POST -d '{"id":"2","address":"10.0.0.3","port":"8","pool_id":"1"}' http://localhost:8080/quantum/v1.0/members/
-
