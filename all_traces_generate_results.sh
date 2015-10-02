77b63e403297866869a2f72189aa5feb01976863
Jeremie Miserez <jeremie@miserez.org>
2015-10-02 14:32:05 +0200

    Add second argument for custom pattern matching.

--- all_traces_generate_results.sh
+++ all_traces_generate_results.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-if [ "$#" -ne 1 ]
+if [ "$#" -lt 1 ]
 then
-  echo "Usage: ./all_traces_generate_results.sh <sdnracer-traces folder path>"
+  echo "Usage: ./all_traces_generate_results.sh <sdnracer-traces folder path> {<pattern for matching directories, e.g. \"trace_*\">}"
   exit 1
 fi
+
+# set default value if not set
+MATCHPATTERN=${2:-"trace_*"}
 
 SCRIPT=$(readlink -f $0)
 SCRIPTPATH=`dirname $SCRIPT`
@@ -24,7 +27,12 @@ export -f generate_results
 trace_dirs_array=()
 while IFS=  read -r -d $'\0'; do
     trace_dirs_array+=("$REPLY")
-done < <(find "$WORKSPACE" -maxdepth 1 -type d -name "trace_*" -print0 | sort -nz)
+done < <(find "$WORKSPACE" -maxdepth 1 -type d -name "$MATCHPATTERN" -print0 | sort -nz)
+
+echo "List of directories to be processed:"
+for i in "${trace_dirs_array[@]}"; do
+  echo "  " "$i"
+done;
 
 # Using GNU Parallel, uses N jobs (N=number of cores) by default
 # -k: keep order of input to output
