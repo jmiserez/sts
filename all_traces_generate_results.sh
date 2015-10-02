a46e2b481a46c18b5b5a21e68ecc27b1d1dcd26d
Jeremie Miserez <jeremie@miserez.org>
2015-10-02 18:00:05 +0200

    Add -i option to all_* scripts to allow for processing of a single trace directory. Will also print output as it happens instead of waiting for the job to be finished.

--- all_traces_generate_results.sh
+++ all_traces_generate_results.sh
@@ -1,26 +1,40 @@
 #!/bin/bash
+SCRIPT=$(readlink -f $0)
+SCRIPTPATH=`dirname $SCRIPT`
+SCRIPTNAME=$(basename "$SCRIPT")
+
 if [ "$#" -lt 1 ]
 then
-  echo "Usage: ./all_traces_generate_results.sh <sdnracer-traces folder path> [<pattern for matching directories, default is \"trace_*\">]"
+  echo "Usage: ./$SCRIPTNAME <sdnracer-traces folder path> [<pattern for matching directories, default is \"trace_*\">]"
+  echo "or     ./$SCRIPTNAME -i <single trace folder path>"
   exit 1
 fi
 
-# set default value if not set
-MATCHPATTERN=${2:-"trace_*"}
-
-SCRIPT=$(readlink -f $0)
-SCRIPTPATH=`dirname $SCRIPT`
-
-WORKSPACE=$1
-echo "WORKSPACE: $WORKSPACE"
-
-# get trace directories
 trace_dirs_array=()
-while IFS=  read -r -d $'\0'; do
-    trace_dirs_array+=("$REPLY")
-done < <(find "$WORKSPACE" -maxdepth 1 -type d -name "$MATCHPATTERN" -print0 | sort -nz)
+ADDITIONAL_PARALLEL_OPTS=""
+case "$1" in
+  -i)
+    if [ "$#" -eq 2 ]
+    then
+      trace_dirs_array+=("$2")
+      ADDITIONAL_PARALLEL_OPTS="--ungroup"
+    else
+      echo "No trace directory specified for option -i"
+      exit 1
+    fi
+    ;;
+  *)
+    WORKSPACE=$1
+    # set default value if not set
+    MATCHPATTERN=${2:-"trace_*"}
+    # get trace directories
+    while IFS=  read -r -d $'\0'; do
+      trace_dirs_array+=("$REPLY")
+    done < <(find "$WORKSPACE" -maxdepth 1 -type d -name "$MATCHPATTERN" -print0 | sort -nz)
+    ;;
+esac
 
-echo "List of directories to be processed:"
+echo "Directories to be processed:"
 for i in "${trace_dirs_array[@]}"; do
   echo "  " "$i"
 done;
@@ -36,5 +50,5 @@ export -f generate_results
 
 # Using GNU Parallel, uses N jobs (N=number of cores) by default
 # -k: keep order of input to output
-parallel -k generate_results ::: "${trace_dirs_array[@]}"
+parallel -k $ADDITIONAL_PARALLEL_OPTS generate_results ::: "${trace_dirs_array[@]}"
 
