440d0e220741cc91e7c172ece359380be4c9ba5f
Jeremie Miserez <jeremie@miserez.org>
2015-10-02 13:48:07 +0200

    Parallelize jenkins autobuild, runs gen.sh on as many cores as there are using GNU Parallel

--- tools/jenkins_autobuild_and_add.sh
+++ tools/jenkins_autobuild_and_add.sh
@@ -7,22 +7,41 @@ fi
 
 SCRIPT=$(readlink -f $0)
 SCRIPTPATH=`dirname $SCRIPT`
-cd "../$SCRIPTPATH"
+cd "$SCRIPTPATH/.."
 
 WORKSPACE=$1
 echo "WORKSPACE: $WORKSPACE"
 
-for i in "$WORKSPACE"/trace_pox_l2_multi-StarTopology2-steps1*; do
-echo "./gen.sh $i"
-./gen.sh "$i"
-pushd "$i"
-# add results
-git add \results_*.dat
-git add \*summary.csv
-# add timings
-git add \timings_*.dat
-git add \*summary_timings.csv
-git status
-popd
-done;
+process_traces() {
+#  echo "Process trace in $1"
+  echo "./gen.sh $1"
+  ./gen.sh "$1"
+}
+export -f process_traces
 
+add_results_to_git(){
+  echo "Add to git in $1"
+  pushd "$1" > /dev/null
+  # add results
+  git add \results_*.dat
+  git add \*summary.csv
+  # add timings
+  git add \timings_*.dat
+  git add \*summary_timings.csv
+  popd > /dev/null
+}
+export -f add_results_to_git
+
+# get trace directories
+trace_dirs_array=()
+while IFS=  read -r -d $'\0'; do
+    trace_dirs_array+=("$REPLY")
+done < <(find "$WORKSPACE" -maxdepth 1 -type d -name "trace_*" -print0 | sort -nz)
+
+# Using GNU Parallel, uses N jobs (N=number of cores) by default
+# -k: keep order of input to output
+parallel -k process_traces ::: "${trace_dirs_array[@]}"
+parallel -k --jobs 1 add_results_to_git ::: "${trace_dirs_array[@]}"
+
+cd "$WORKSPACE"
+git status
