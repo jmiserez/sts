2043a4d0d5aa8dca798349447bce925b78ac55c7
Jeremie Miserez <jeremie@miserez.org>
2015-10-28 10:12:19 +0100

    Add config file for Floodlight v1.1+

---
+++ config/floodlight_loxigen.py
@@ -0,0 +1,66 @@
+from config.experiment_config_lib import ControllerConfig
+from sts.topology import StarTopology, BufferedPatchPanel, MeshTopology, GridTopology, BinaryLeafTreeTopology
+from sts.controller_manager import UserSpaceControllerPatchPanel
+from sts.control_flow.fuzzer import Fuzzer
+from sts.control_flow.interactive import Interactive
+from sts.input_traces.input_logger import InputLogger
+from sts.simulation_state import SimulationConfig
+from sts.happensbefore.hb_logger import HappensBeforeLogger
+from config.application_events import AppCircuitPusher
+
+start_cmd = ('''java -ea -Dlogback.configurationFile=./src/main/resources/logback-test-loxi-trace.xml -jar '''
+             '''./target/floodlight.jar '''
+              '''-cf ./src/main/resources/floodlightdefault_loxi_local.properties''')
+
+# controllers = [ControllerConfig(start_cmd, cwd='../../floodlight/floodlight', address="127.0.0.1", port=6653)]
+
+# Uncomment this if you are running Floodlight separately, e.g. for debugging in Eclipse. There must be a controller listening on port 6633.
+start_cmd = '''echo "no-op"'''
+controllers = [ControllerConfig(start_cmd, cwd='../../floodlight/floodlight', address="127.0.0.1", port=6653, controller_type='dummy')]
+
+num = 2
+topology_class = StarTopology
+topology_params = "num_hosts=%d" % num
+#topology_class = MeshTopology
+#topology_params = "num_switches=%d" % num
+# topology_class = GridTopology
+# topology_params = "num_rows=3, num_columns=3"
+# topology_class = BinaryLeafTreeTopology
+# topology_params = "num_levels=%d" % num
+
+steps = 400
+results_dir = "traces/floodlight_loxigen-%s%d-steps%s" % (topology_class.__name__, num, steps)
+
+apps = []
+
+simulation_config = SimulationConfig(controller_configs=controllers,
+                                     topology_class=topology_class,
+                                     topology_params=topology_params,
+                                     patch_panel_class=BufferedPatchPanel,
+                                     controller_patch_panel_class=UserSpaceControllerPatchPanel,
+                                     dataplane_trace=None,
+                                     snapshot_service=None,
+                                     multiplex_sockets=False,
+                                     violation_persistence_threshold=None,
+                                     kill_controllers_on_exit=True,
+                                     interpose_on_controllers=False,
+                                     ignore_interposition=False,
+                                     hb_logger_class=HappensBeforeLogger,
+                                     hb_logger_params=results_dir,
+                                     apps=apps)
+
+# Manual, interactive mode
+# control_flow = Interactive(simulation_config, input_logger=InputLogger())
+
+control_flow = Fuzzer(simulation_config,
+                      input_logger=InputLogger(),
+                      initialization_rounds=20,
+                      send_all_to_all=True, # needs to be True otherwise circuitpusher will throw errors.
+                      check_interval=10,
+                      delay=0.1,
+                      halt_on_violation=True,
+                      send_init_packets=False,
+                      steps=steps, # if no circuits are installed, increase this number.
+#                       invariant_check_name="check_everything",
+                      invariant_check_name="InvariantChecker.check_liveness",
+                      apps=apps)
