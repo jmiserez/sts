jmiserez/sts

commit 30dcbff74c68f8d5ebf0087e7986232aabc51ac1

wip on support for large traces

sts/entities/sts_entities.py
@@ -219,7 +219,7 @@ class TracingSwitchFlowTable(SwitchFlowTable, EventMixin):
         self.raiseEvent(TraceAsyncSwitchFlowExpiryBegin(self.switch.dpid))
         duration_sec, duration_nsec = entry.duration_sec_nsec(now)
         flow_mod = entry.to_flow_mod()
-        self.raiseEvent(TraceSwitchFlowTableEntryExpiry(self.switch.dpid, self, flow_mod, duration_sec, duration_nsec, reason))
+        self.raiseEvent(TraceSwitchFlowTableEntryExpiry(self.switch.dpid, flow_mod, duration_sec, duration_nsec, reason, flow_table=self))
         self.table.remove(entry)
         # NOTE(jm): We send one event for *each* removal, instead of grouping them together.
         self.raiseEvent(FlowTableModification(removed=[entry], reason=reason, now=now))
@@ -251,7 +251,7 @@ class TracingSwitchFlowTable(SwitchFlowTable, EventMixin):
     """ Process a flow mod sent to the switch
     @return a tuple (added|modified|removed, [list of affected entries])
     """
-    self.raiseEvent(TraceSwitchFlowTableWrite(self.switch.dpid, self, flow_mod))
+    self.raiseEvent(TraceSwitchFlowTableWrite(self.switch.dpid, flow_mod, flow_table=self))
     return super(TracingSwitchFlowTable, self).process_flow_mod(flow_mod)
 
 class TracingNXSoftwareSwitch(NXSoftwareSwitch, EventMixin):
@@ -348,14 +348,14 @@ class TracingNXSoftwareSwitch(NXSoftwareSwitch, EventMixin):
     if(entry != None):
       now = time.time()
       plen = len(packet)
-      self.raiseEvent(TraceSwitchFlowTableRead(self.dpid, packet, in_port, self.table, entry, plen, now))
+      self.raiseEvent(TraceSwitchFlowTableRead(self.dpid, packet, in_port, entry, plen, now, flow_table=self.table))
       entry.touch_packet(plen, now)
       self._process_actions_for_packet(entry.actions, packet, in_port)
     else:
       if cause_is_ofpp_table:
         self.log.warn("Switch-controller recursion: out_port is OFPP_TABLE, but no entry was found and packet is going back to switch as packet in.")
       # no matching entry
-      self.raiseEvent(TraceSwitchFlowTableRead(self.dpid, packet, in_port, self.table, None, None, None))
+      self.raiseEvent(TraceSwitchFlowTableRead(self.dpid, packet, in_port, None, None, None, flow_table=self.table))
       buffer_id = self._buffer_packet(packet, in_port)
       self.send_packet_in(in_port, buffer_id, packet, self.xid_count.next(), reason=OFPR_NO_MATCH)
   

sts/happensbefore/hb_sts_events.py
@@ -32,7 +32,7 @@ class TraceSwitchEvent(JsonEvent):
                     ('out_port', get_port_no),
                     'buffer_id',
                     ('msg', base64_encode),
-                    ('flow_table', base64_encode_flow_table),
+                    ('flow_table', lambda x: x if x is None else base64_encode_flow_table(x)),
                     ('flow_mod', base64_encode),
                     ('removed', base64_encode),
                     ('expired_flows', base64_encode_flow_list),
@@ -55,7 +55,7 @@ class TraceSwitchEvent(JsonEvent):
     'out_port': lambda x: x,
     'buffer_id': lambda x: x,
     'msg': base64_decode_openflow,
-    'flow_table': decode_flow_table,
+    'flow_table': lambda x: None if x is None else decode_flow_table(x),
     'flow_mod': decode_flow_mod,
     'removed': decode_flow_mod,
     'expired_flows': lambda flows: [decode_flow_mod(x) for x in flows],
@@ -155,13 +155,13 @@ class TraceSwitchPacketSend(TraceSwitchEvent):
 
 
 class TraceSwitchFlowTableRead(TraceSwitchEvent):
-  def __init__(self, dpid, packet, in_port, flow_table, flow_mod,
+  def __init__(self, dpid, packet, in_port, flow_mod, flow_table=None,
                touched_flow_bytes=None, touched_flow_now=None, t=None, eid=None):
     TraceSwitchEvent.__init__(self, t=t, eid=eid)
     self.dpid = dpid
     self.packet = packet
     self.in_port = in_port
-    self.flow_table = decode_flow_table(base64_encode_flow_table(flow_table, set_zero_XID=True))
+    self.flow_table = None if flow_table is None else decode_flow_table(base64_encode_flow_table(flow_table, set_zero_XID=True))
     self.flow_mod = decode_flow_mod(base64_encode_flow(flow_mod, set_zero_XID=True))
     self.entry = decode_flow_mod(base64_encode_flow(flow_mod, set_zero_XID=True)) #TODO(jm): unused
     self.touched_flow_bytes = touched_flow_bytes
@@ -169,17 +169,17 @@ class TraceSwitchFlowTableRead(TraceSwitchEvent):
 
 
 class TraceSwitchFlowTableWrite(TraceSwitchEvent):
-  def __init__(self, dpid, flow_table, flow_mod, t=None, eid=None):
+  def __init__(self, dpid, flow_mod, flow_table=None, t=None, eid=None):
     TraceSwitchEvent.__init__(self, t=t, eid=eid)
     self.dpid = dpid
-    self.flow_table = decode_flow_table(base64_encode_flow_table(flow_table, set_zero_XID=True))
+    self.flow_table = None if flow_table is None else decode_flow_table(base64_encode_flow_table(flow_table, set_zero_XID=True))
     self.flow_mod = decode_flow_mod(base64_encode_flow(flow_mod))
     
 class TraceSwitchFlowTableEntryExpiry(TraceSwitchEvent):
-  def __init__(self, dpid, flow_table, flow_mod, duration_sec, duration_nsec, reason, t=None, eid=None):
+  def __init__(self, dpid, flow_mod, duration_sec, duration_nsec, reason, flow_table=None, t=None, eid=None):
     TraceSwitchEvent.__init__(self, t=t, eid=eid)
     self.dpid = dpid
-    self.flow_table = decode_flow_table(base64_encode_flow_table(flow_table, set_zero_XID=True))
+    self.flow_table = None if flow_table is None else decode_flow_table(base64_encode_flow_table(flow_table, set_zero_XID=True))
     self.flow_mod = decode_flow_mod(base64_encode_flow(flow_mod, set_zero_XID=True))
     self.duration_sec = duration_sec
     self.duration_nsec = duration_nsec

sts/happensbefore/hb_race_detector.py
@@ -265,6 +265,7 @@ class RaceDetector(object):
       self.apply_time_filter(self.rw_delta)
 
     # Calculate true harmful
+    # TODO(jm): This line of code eats memory like crazy, and takes forever! Change this!
     self._races_harmful = [race for race in self.found_races_harmful if race not in self.filtered_by_time]
 
     self.racing_events_harmful = set()

sts/happensbefore/hb_shadow_table.py
@@ -19,9 +19,10 @@ from hb_utils import find_entries_in_flow_table
 
 class ShadowFlowTable(object):
 
-  def __init__(self, dpid):
+  def __init__(self, dpid, is_minimized_trace=False):
     self._ids = itertools.count(0)
     self.dpid = dpid
+    self.is_minimized_trace = is_minimized_trace
     self.table = SwitchFlowTable()
     
     self.latest_event_eid = None
@@ -110,7 +111,8 @@ class ShadowFlowTable(object):
     assert hasattr(event, "operations")
     for op in event.operations:
       if type(op) in [TraceSwitchFlowTableRead, TraceSwitchFlowTableWrite, TraceSwitchFlowTableEntryExpiry]:
-        assert hasattr(op, "flow_table")
+        if not self.is_minimized_trace:
+          assert hasattr(op, "flow_table")
         assert hasattr(op, "flow_mod")
         if type(op) == TraceSwitchFlowTableRead:
           self.latest_event_was_async_expiry = False
@@ -118,7 +120,8 @@ class ShadowFlowTable(object):
           assert hasattr(op, "in_port")
           assert hasattr(op, "entry")
           # shadow table should agree with trace before op
-          assert compare_flow_table(self.table, op.flow_table)
+          if not self.is_minimized_trace:
+            assert compare_flow_table(self.table, op.flow_table)
           
           entry = read_flow_table(self.table, event.packet, event.in_port)
           
@@ -144,16 +147,18 @@ class ShadowFlowTable(object):
         elif type(op) == TraceSwitchFlowTableWrite:
           self.latest_event_was_async_expiry = False
           # shadow table should agree with trace before op
-          assert compare_flow_table(self.table, op.flow_table)
+          if not self.is_minimized_trace:
+            assert compare_flow_table(self.table, op.flow_table)
           write_flow_table(self.table, op.flow_mod)
 
         elif type(op) == TraceSwitchFlowTableEntryExpiry:
           self.latest_event_was_async_expiry = True
-          if not compare_flow_table(self.table, op.flow_table):
-            print self.table.table
-            print "--------------------"
-            print op.flow_table.table
-          assert compare_flow_table(self.table, op.flow_table)
+          if not self.is_minimized_trace:
+            if not compare_flow_table(self.table, op.flow_table):
+              print self.table.table
+              print "--------------------"
+              print op.flow_table.table
+            assert compare_flow_table(self.table, op.flow_table)
           exact_matches = find_entries_in_flow_table(self.table, op.flow_mod)
           # it is impossible to add two entries with the *exact* same match and
           # priority to the flow table, so we should always get exactly one entry

commit 182c86dc2ae8ae04868f03f4e8af90eaf7457ca6

fix mismatched constructor call for TraceSwitchFlowTableRead. Bug was originally introduced in 30dcbff74c68f8d5ebf0087e7986232aabc51ac, where the goal was to make specifying the flow_table parameter optional to support processing traces without serialized flow_table fields.

sts/entities/sts_entities.py
@@ -348,14 +348,14 @@ class TracingNXSoftwareSwitch(NXSoftwareSwitch, EventMixin):
     if(entry != None):
       now = time.time()
       plen = len(packet)
-      self.raiseEvent(TraceSwitchFlowTableRead(self.dpid, packet, in_port, entry, plen, now, flow_table=self.table))
+      self.raiseEvent(TraceSwitchFlowTableRead(self.dpid, packet, in_port, entry, touched_flow_bytes=plen, touched_flow_now=now, flow_table=self.table))
       entry.touch_packet(plen, now)
       self._process_actions_for_packet(entry.actions, packet, in_port)
     else:
       if cause_is_ofpp_table:
         self.log.warn("Switch-controller recursion: out_port is OFPP_TABLE, but no entry was found and packet is going back to switch as packet in.")
       # no matching entry
-      self.raiseEvent(TraceSwitchFlowTableRead(self.dpid, packet, in_port, None, None, None, flow_table=self.table))
+      self.raiseEvent(TraceSwitchFlowTableRead(self.dpid, packet, in_port, None, flow_table=self.table))
       buffer_id = self._buffer_packet(packet, in_port)
       self.send_packet_in(in_port, buffer_id, packet, self.xid_count.next(), reason=OFPR_NO_MATCH)
   

sts/happensbefore/hb_sts_events.py
@@ -155,8 +155,8 @@ class TraceSwitchPacketSend(TraceSwitchEvent):
 
 
 class TraceSwitchFlowTableRead(TraceSwitchEvent):
-  def __init__(self, dpid, packet, in_port, flow_mod, flow_table=None,
-               touched_flow_bytes=None, touched_flow_now=None, t=None, eid=None,
+  def __init__(self, dpid, packet, in_port, flow_mod,
+               touched_flow_bytes=None, touched_flow_now=None, flow_table=None, t=None, eid=None,
                make_copy=True):
     TraceSwitchEvent.__init__(self, t=t, eid=eid)
     self.dpid = dpid